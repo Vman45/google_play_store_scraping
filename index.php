8ecda4a9356953fdf862be354624bdf6d4aead2f
Alex Melehy <amelehy@gmail.com>
2017-09-18 17:04:57 -0400

    done

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
+  
+  // Set the max execution time in php.ini to something larger than 30 seconds (default)
+  ini_set('max_execution_time', 500);
 
   include('google_store_scraper.php');
   
   $dataScraped = array();
   $scraper = new GoogleStoreScraper();
 
-  $csv = array_map('str_getcsv', file('urls.csv' , FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES));
+  $csv = array_map('str_getcsv', file('google_play_urls.csv' , FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES));
 
   foreach ($csv as $group) {
     foreach ($group as $url) {
